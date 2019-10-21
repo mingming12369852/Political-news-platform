d0f1e8eff43f0013d0c2297e4189339ba3d27c82
mingming12369852 <mingming12369852@gmail.com>
2019-10-21 17:21:41 +0800

    v1.41a 02

--- src/nods02.js
+++ src/nods02.js
@@ -15,6 +15,8 @@ function startServer() {
 
   const server = http.createServer((req, res) => {
     res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin',"*")
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
     res.setHeader("Content-Type", "application/json; charset=utf-8");
     console.log("HI");
     var Alldata = date;
